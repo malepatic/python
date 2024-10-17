46d9233a1bbb0fb7d86b963421df89629e78454f
malepatic <malepati.c@northeastern.edu>
2024-10-17 18:30:04 -0400

    message

---
+++ cal.py
@@ -0,0 +1,151 @@
+###Tip #######
+# print("Welcome to the tip calculator")
+# bill =int( input("what was the total bill? $"))
+# ttip = int(input("How much tip would you like to give? 10 12 15"))
+# people =int( input("how many people? "))
+#
+# tip = bill * (1 + ttip / 100)
+#
+# print(tip)
+
+###########String methods#########
+# first = "chaitanya"
+# last ="Malepati"
+# msg = f"{first} [{last}] is a QA engineer"
+# print(len(msg))
+# first.upper()
+# print(msg.replace("QA","CLOUD"))
+# print("chaitanya" in first)
+
+#####Arithmetic operations#####
+
+###math functions###
+
+# import math
+#
+# print(math.pi(3.14))
+
+####conditional statement####
+# if
+# is_hot = False
+# is_cold = False
+# if is_hot :
+#     print("It is a hot day")
+#     print("Drink energy drink")
+# elif is_cold:
+#     print("It is a cold day")
+#     print("Wear jackets")
+# else:
+#     print("It is a lovely day")
+# print("enjoy ur day")
+# price = 1000000
+# has_good_credit = True
+# if has_good_credit :
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+# print(f"down payment: ${down_payment}")
+
+
+### logical operators ######
+#and : both
+# or : at least one
+# Not
+
+# has_high_income = False
+# has_good_credit = True
+# if has_high_income and not has_good_credit:
+#     print("eligibe")
+# else:
+#     print("not")
+
+
+### Comparision ###
+# > , < , => , =< , == , !=
+
+# temperature = 30
+#
+# if temperature == 30 :
+#     print("it is a hot day")
+# else:
+#     print("temp")
+
+# name = "chaitanya"
+# len(name)
+#
+# if len(name) < 3 :
+#     print("name should be more than 3 characters")
+# elif len(name) > 50:
+#     print("name should be maximum of 50 characters")
+# else:
+#     print("your name is good!")
+
+#PROJECT : WEIGHT CONVERTER PROGRAM
+
+# weight = int(input("Weight: "))
+# weight_scale = input("(L)bs or (K)gs :")
+# if weight_scale.upper() == "L":
+#    converted =  weight * 0.45
+#    print(f"you are {converted} kilos")
+# else:
+#     converted2 = weight / 0.45
+#     print(f"you are {converted2} pounds")
+
+### While loop ##
+
+# i = 1
+# while i <= 5 :
+#     print("*" * i)
+#     i = i + 1
+# print("done!")
+
+## guessing number ##
+
+# secret_number = 8
+# guess_count = 0
+# guess_limit = 3
+# while guess_count < guess_limit :
+#     guess = int(input("Guess: "))
+#     guess_count += 1
+#     if guess == secret_number:
+#         print("you win!")
+#         break
+# else:
+#     print("You are failed try again :)")
+
+#car game
+
+# 1:31
+
+
+# weight = int(input("Enter your weight: "))
+# label = input("(L)gs or (K)bs: ")
+# if label.upper() == "L":
+#     conversion = weight * 0.2
+#     print(f"your converted weight is {conversion}")
+# else:
+#     conversion2 = weight / 0.45
+#     print(f"your converted weight is {conversion2}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,65 @@
+# name1 = input("what is your name: ")
+# name2 = input("what is ur fav color: ")
+# print(name1 + " likes " + name2)
+#
+#
+# birth_year = int(input("what is ur birth year:"))
+# print(birth_year)
+# string= "chaitanya"
+# print(string[5:5])
+# name = "chaitanya"
+# print(name.title())
+# chai_marks = 70
+# if chai_marks > 95 :
+#     print("GOT A  GRADE")
+# elif chai_marks == 95 :
+#     print("GOT A- GRADE")
+#
+# else:
+#     print("GOT B- GRADE")
+# int = 90.6
+# print(round(int))
+# import numbers
+#numbers = 1,2,3,4
+#print(numbers[0])
+
+# class Solution():
+#     def __class__(self, nums, target):
+# chai_marks = input("How many marks you got? ")
+# if input == 100:
+#     print("you are pass")
+# else:
+#     print("concentrate in the things")
+
+# print("You got " + input("Enter your marks? ") + " marks!")
+# print(len(input("what is your name? ")))
+# username = input("what is ur name / full name? ")
+# length = len(username)
+# print(length)
+
+# a = 65
+# b = -35
+# temp = a
+# a = b
+# temp = b
+# print(a+b)
+
+# print("welcome to the band generator")
+# a = input("What's the name of the city you grew up in? \n")
+# pet_name = input("what's your pet's name \n")
+# print("your band name could be: " + a + " " + pet_name)
+
+# print("welcome to the calculator")
+# first_name = input("what is ur name? \n")
+# num1 = 25
+# num2 = 2
+# operator = num1 + num2   #input("enter the action you want to perform: ")
+# print(operator)
+
+# print("welcome to jvue")
+# user_name = input("what is your name? \n")
+# apt_number = input("what is the apt number? \n")
+# print(user_name + " " + "lives in " + apt_number)
+
+print(len(input("are you chaitanya? ")))
+
